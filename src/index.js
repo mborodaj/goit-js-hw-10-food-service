d5ad7f23f5c6f32a93900f7ee2981c5adcd11e06
Mikhail Borodai <boroday.mihail@gmail.com>
2020-12-03 20:50:04 +0200

    initial commitment

--- src/index.js
+++ src/index.js
@@ -3,17 +3,33 @@ import menu from "./menu.json";
 import itemsTemplates from "./templates/menu-items.hbs";
 
 const menuRef = document.querySelector(".js-menu");
-const themeSwitcherRef = document.querySelector(".theme-switch__toggle");
-
-
-
-
-console.dir(themeSwitcherRef.checked);
+const themeSwitcherRef = document.getElementById("theme-switch-toggle");
+const bodyRef = document.querySelector("body");
 
 const markup = itemsTemplates(menu);
 
 menuRef.insertAdjacentHTML("beforeend", markup);
 
-if (themeSwitcherRef.value === "On") {
-  console.log("On");
+setTheme();
+
+themeSwitcherRef.addEventListener("change", changeTheme);
+
+function changeTheme() {
+  if (this.checked) {
+    bodyRef.setAttribute("class", "dark-theme");
+    localStorage.setItem("theme", "dark-theme");
+  } else {
+    bodyRef.setAttribute("class", "light-theme");
+    localStorage.setItem("theme", "light-theme");
+  }
+  localStorage.setItem("switcherChecked", this.checked);
+}
+
+function setTheme() {
+  document.body.classList.remove("dark-theme");
+  document.body.classList.remove("light-theme");
+  document.body.classList.add(localStorage.getItem("theme"));
+  themeSwitcherRef.checked = JSON.parse(
+    localStorage.getItem("switcherChecked"),
+  );
 }
